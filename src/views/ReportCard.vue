03d116768a54ab8e9a297fb7659608d2c484ade6
肖晨亮 <xiao_chen_liang@qq.com>
2024-04-13 11:32:35 +0800

    verify pdf files

--- src/views/ReportCard.vue
+++ src/views/ReportCard.vue
@@ -5,22 +5,20 @@
       action="http://localhost:5000/upload"
       :on-preview="handlePreview"
       :on-remove="handleRemove"
+      :before-upload="beforeUpload"
       :file-list="fileList"
       multiple>
     <i class="el-icon-upload"></i>
     <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
-    <div class="el-upload__tip" slot="tip">jpg/png files with a size less than 500kb</div>
+    <div class="el-upload__tip" slot="tip">PDF files with a size less than 500kb</div>
   </el-upload>
 </template>
 
-
 <script>
-
 export default {
   data() {
     return {
-      fileList: [
-      ]
+      fileList: []
     };
   },
   methods: {
@@ -29,8 +27,19 @@ export default {
     },
     handlePreview(file) {
       console.log(file);
+    },
+    beforeUpload(file) {
+      const isPDF = file.type === 'application/pdf';
+      const isLt500K = file.size / 1024 < 500;
+
+      if (!isPDF) {
+        this.$message.error('Please upload PDF file only');
+      }
+      if (!isLt500K) {
+        this.$message.error('File size should be less than 500KB');
+      }
+      return isPDF && isLt500K;
     }
   }
 }
-
 </script>
