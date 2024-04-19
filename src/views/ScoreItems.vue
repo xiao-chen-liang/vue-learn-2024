5ed89f374cd5c398054a271e633928253130fc4e
肖晨亮 <xiao_chen_liang@qq.com>
2024-04-19 09:55:55 +0800

    half work to control the rule

--- src/views/ScoreItems.vue
+++ src/views/ScoreItems.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <button @click="getData">Fetch Data</button>
-    <div v-if="data">
-      <h2>Data:</h2>
-      <pre>{{ data }}</pre>
-    </div>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          <el-cascader :options="options" clearable @change="handleCascaderChange"></el-cascader>
+        </div>
+      </el-col>
+      <el-col :span="16">
+        <div class="grid-content bg-purple">
+          <!-- Display the data stored in the rule variable -->
+          <pre>{{ rule }}</pre>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -14,19 +22,41 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      data: null
+      data: null,
+      options: null,
+      rule: null, // Define the rule variable
     };
   },
   methods: {
-    async getData() {
-      console.log('ScoreItems.vue getData()');
+    async getOptions() {
       try {
-        const response = await axios.get('http://127.0.0.1:5000/me');
-        this.data = response.data;
+        const response = await axios.get('http://localhost:5000/get_grades_and_colleges');
+        return response.data;
       } catch (error) {
         console.error('Error:', error);
       }
+    },
+    async handleCascaderChange(selectedValues) {
+      try {
+        // Append the selected value to the request URL
+        const url = `http://localhost:5000/get_rule_data/${selectedValues[0]}/${selectedValues[1]}`;
+        const response = await axios.get(url);
+        // Save the fetched data to the rule variable
+        this.rule = response.data;
+        console.log('Rule data:', this.rule)
+      } catch (error) {
+        console.error('Error fetching rule data:', error);
+      }
     }
+  },
+  async created() {
+    this.options = await this.getOptions();
   }
 };
 </script>
+
+<style scoped>
+/* Your scoped styles here */
+</style>
+
+
